dd3d0c1d8cb0ce7c26b33392b3894cb6771b20f6
hai phan <phanthanhhai07t1@gmail.com>
2018-04-02 00:18:28 +0700

    Table(model) structure

---
+++ db/migrate/20180401092053_create_developer_programming_languages.rb
@@ -0,0 +1,10 @@
+class CreateDeveloperProgrammingLanguages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :developer_programming_languages do |t|
+      t.integer :developer_id
+      t.integer :programming_language_id
+
+      t.timestamps
+    end
+  end
+end
