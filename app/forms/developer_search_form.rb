0070d71809812c8fc34f7ed20fbaec832a8fdbc6
hai phan <phanthanhhai07t1@gmail.com>
2018-04-11 09:30:23 +0700

    Make changes according to review

--- app/forms/developer_search_form.rb
+++ app/forms/developer_search_form.rb
@@ -9,20 +9,14 @@ class DeveloperSearchForm
   attribute :programming_language_id, Integer
 
   def search
-    query = Developer.all
-    if language_id.present?
-      query = query.includes(:developer_languages)
-                   .joins(:developer_languages)
-                   .where(developer_languages: { language_id: language_id })
-    end
+    query = Developer.includes(:developer_languages,
+                               :languages,
+                               :developer_programming_languages,
+                               :programming_languages)
 
-    if programming_language_id.present?
+    query = query.where(developer_languages: { language_id: language_id }) if language_id.present?
 
-      query = query.includes(:developer_programming_languages)
-                   .joins(:developer_programming_languages)
-                   .where(developer_programming_languages: { programming_language_id: programming_language_id })
-
-    end
+    query = query.where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
 
     query.order(created_at: :desc)
   end
